9a118e1bb192703242a1df9ccc379e754bf8cb65
SlotSun <s1059819898@gmail.com>
2020-01-21 00:25:20 +0800

    +++

--- src/main/java/life/slot/community/controller/AuthorizeController.java
+++ src/main/java/life/slot/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.slot.community.dto.AccessTokenDTO;
 import life.slot.community.dto.GithubUser;
 import life.slot.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${Client.secret}")
+    private String clientSecret;
+
+    @Value("${github.Redirect.uri}")
+    private String redirectUri;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("8b636b7576db82761c08");
-        accessTokenDTO.setClient_secret("099ad3d55b333fdaecb95a6795260e537bb59ba9");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
