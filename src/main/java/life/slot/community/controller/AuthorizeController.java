e24aa2c11a00c8028168fdbf69e9569c3357006b
SlotSun <s1059819898@gmail.com>
2020-02-16 03:02:28 +0800

    update README

--- src/main/java/life/slot/community/controller/AuthorizeController.java
+++ src/main/java/life/slot/community/controller/AuthorizeController.java
@@ -57,7 +57,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            user.setAvatarUrl(githubUser.getAvatar_url());
+            user.setAvatarUrl(githubUser.getAvatarUrl());
             userMapper.insert(user);
             request.getSession().setAttribute("githubUser", githubUser);
             response.addCookie(new Cookie("token", token));
--- src/main/java/life/slot/community/dto/GithubUser.java
+++ src/main/java/life/slot/community/dto/GithubUser.java
@@ -7,7 +7,7 @@ public class GithubUser {
     private String name;
     private long id;
     private String bio;
-    private String avatar_url;
+    private String avatarUrl;
 
     @Override
     public String toString() {
