7298194c575b68babe92a2f1aa110e17fb950bdf
SlotSun <s1059819898@gmail.com>
2020-02-17 02:04:29 +0800

    add 分页

---
+++ src/main/java/life/slot/community/dto/PaginationDTO.java
@@ -0,0 +1,49 @@
+package life.slot.community.dto;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by slot on 2020-02-16
+ */
+@Data
+public class PaginationDTO {
+    private List<QuestionDTO> questions;
+    private boolean showPrevious;
+    private boolean showFirstPage;
+    private boolean showNext;
+    private boolean showEndPage;
+    private Integer page;
+    private List<Integer> pages = new ArrayList<>();
+    private Integer totalPage;
+
+    public void setPagination(Integer totalCount, Integer page, Integer size) {
+        totalPage = totalCount % size == 0 ? totalCount / size : totalCount / size + 1;
+        if (page < 1) {
+            page = 1;
+        }
+        if (page >totalPage) {
+            page = totalPage;
+        }
+        //页面控制
+        this.page = page;
+        pages.add(page);
+        for (int i = 1; i <= 3; i++) {
+            if (page - i > 0) {
+                pages.add(0, page - i);
+            }
+            if (page + i <= totalPage) {
+                pages.add(page + i);
+
+            }
+        }
+
+
+        showPrevious = page == 1 ? false : true;
+        showNext = page == totalPage ? false : true;
+        showFirstPage = pages.contains(1) ? false : true;
+        showEndPage = pages.contains(totalPage) ? false : true;
+    }
+}
--- src/main/java/life/slot/community/service/QuestionService.java
+++ src/main/java/life/slot/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.slot.community.service;
 
+import life.slot.community.dto.PaginationDTO;
 import life.slot.community.dto.QuestionDTO;
 import life.slot.community.mapper.QuestionMapper;
 import life.slot.community.mapper.UserMapper;
@@ -24,9 +25,22 @@ public class QuestionService {
     @Autowired(required = false)
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -35,7 +49,9 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
+        paginationDTO.setQuestions(questionDTOList);
 
-        return questionDTOList;
+
+        return paginationDTO;
     }
 }
